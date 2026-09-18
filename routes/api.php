171e40fe99b512372a24f35883c26afc7c186205
exacoresandeep <sandeep.ps@exacoreitsolutions.com>
2026-09-18 10:30:54 +0530

    new page

--- routes/api.php
+++ routes/api.php
@@ -64,7 +64,7 @@ Route::post('/sap/downloadLedger', [\App\Http\Controllers\SAPController::class, 
                     Route::post('/', [DealerOrderController::class, 'store']); // Store  order
                     Route::get('/', [DealerOrderController::class, 'index']); // List orders by current user ID
                     Route::get('/{orderId}', [DealerOrderController::class, 'show']); // order details
-                    Route::get('delete/{orderId}', [DealerOrderController::class, 'deleteOrder']); 
+                    Route::post('delete/{orderId}', [DealerOrderController::class, 'deleteOrder']); 
                     // Route::post('/filter', [DealerOrderController::class, 'orderFilter']);
                     Route::post('/track-order', [DealerOrderController::class, 'trackOrder']);
                     Route::post('/monthly-transaction', [DealerOrderController::class, 'monthlySalesTransaction']);
