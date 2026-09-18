320ae2b5c129b8fa8d685c00bd2b6517e1596a0a
exacoresandeep <sandeep.ps@exacoreitsolutions.com>
2026-09-18 14:14:04 +0530

    new page

---
+++ app/Http/Middleware/ApiMaintenance.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiMaintenance
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (env('API_MAINTENANCE', false)) {
+            return response()->json([
+                'success' => false,
+                'statusCode' => 503,
+                'message' => 'API is currently under maintenance. Please try again later.',
+            ], 503);
+        }
+        return $next($request);
+    }
+}
